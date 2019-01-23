25030020fe1165f6de3f0d461a94b5323c481ec5
wyeung46 <wyeung46@my.bcit.ca>
2019-01-22 16:31:45 -0800

    Added top documentation, surface area and coverage calculator

--- my_circle.py
+++ my_circle.py
@@ -1,7 +1,16 @@
-pi = 3.14159
+"""DOCSTRING for Lab 02"""
+#Wendell Yeung
+#A01063806
+#22 January 2019
+
+PI = 3.14159
 radius = 0
 radius = float(input("Enter your radius:\n"))
-calculatedCircumference = 2 * pi * radius
+calculatedCircumference = 2 * PI * radius
 print("The calculated circumference is", calculatedCircumference)
-calculatedArea = float(pi * radius * radius)
+calculatedArea = float(PI * radius * radius)
 print("The calculated area is", calculatedArea)
+double_radius = 2 * radius
+double_circumference = 2 * PI * double_radius
+double_area = PI * double_radius * double_radius
+print("When you double the radius, the circumference increases by a factor of", (double_circumference / calculatedCircumference), "and the area increases by a factor of", (double_area / calculatedArea))
