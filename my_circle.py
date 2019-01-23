973883bdddf195f49cd49d10f89a430d84f5ec62
wyeung46 <wyeung46@my.bcit.ca>
2019-01-22 16:07:50 -0800

    Added circumference printer

--- my_circle.py
+++ my_circle.py
@@ -1,4 +1,5 @@
 pi = 3.14159
 radius = 0
-enteredRadius = float(input("Enter your radius:/n"))
+enteredRadius = float(input("Enter your radius:\n"))
 calculatedCircumference = 2 * pi * enteredRadius
+print("The calculated circumference is", calculatedCircumference)
