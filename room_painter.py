5ab656fbfb00ad0788a507936c8f52d066d4c588
wyeung46 <wyeung46@my.bcit.ca>
2019-01-22 16:32:55 -0800

    Added top documentation, surface area and coverage calculator

--- room_painter.py
+++ room_painter.py
@@ -1,2 +1,15 @@
-def COVERAGE():
-    return 400
+"""DOCSTRING for Week 03 Notes"""
+# Wendell Yeung
+# A01063806
+# 22 January 2019
+
+
+COVERAGE = 400
+length = float(input("Enter your room length in feet. "))
+width = float(input("Enter your room width in feet. "))
+height = float(input("Enter your room height in feet. "))
+coats = float(input("Enter the number of desired coats. "))
+surface_area = 5 * (length * width * height)
+coverage_needed = coats * surface_area
+cans_of_paint_required = coverage_needed / COVERAGE
+print("You will need", cans_of_paint_required, "cans of paint.")
