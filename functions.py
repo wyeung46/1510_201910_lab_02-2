e6f712bbb443a52c9f4f8821890cec344a32b3bc
wyeung46 <wyeung46@my.bcit.ca>
2019-01-22 17:06:03 -0800

    Finished format_name function

---
+++ functions.py
@@ -0,0 +1,22 @@
+"""DOCSTRING for Lab 02"""
+#Wendell Yeung
+#A01063806
+#22 January 2019
+
+def format_name(firstName, lastName):
+    # fullname = str(firstName) + " " + str(lastName)
+    # titledname = fullname.title()
+    # finalname = titledname.strip()
+    firstName = firstName.strip().capitalize()
+    lastName = lastName.strip().capitalize()
+    return (firstName + " " + lastName)
+
+def main():
+    first = input("Enter your first name")
+    last = input("Enter your last name")
+    result = format_name(first, last)
+    print(result)
+
+if __name__ == "__main__":
+    main()
+
